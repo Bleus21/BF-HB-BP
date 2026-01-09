22662f6c2dfc7f37a41112f8dc6190f01f731769
Bleus21 <vinny210482@gmail.com>
2026-01-09 21:51:14 +0100

    Update reposter_core.py

--- reposter_core.py
+++ reposter_core.py
@@ -28,7 +28,7 @@ FEEDS = {
 # === LIJSTEN (max 10) ===
 # link mag: at://.../app.bsky.graph.list/...  OF  https://bsky.app/profile/<actor>/lists/<rkey>
 LIJSTEN = {
-    "lijst 1": {"link": "", "note": ""},
+    "lijst 1": {"link": "https://bsky.app/profile/did:plc:jaka644beit3x4vmmg6yysw7/lists/3m3iga6wnmz2p", "note": "Beautygrouplist"},
     "lijst 2": {"link": "", "note": ""},
     "lijst 3": {"link": "", "note": ""},
     "lijst 4": {"link": "", "note": ""},
